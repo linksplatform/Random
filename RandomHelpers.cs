95dbbe6637298cecc83010d49b70327a5462df73
Константин Дьяченко <konard@me.com>
2019-07-26 19:27:21 +0300

    Use UtcNow.Ticks hash code instead of 32-bit truncated value.

--- RandomHelpers.cs
+++ RandomHelpers.cs
@@ -1,11 +1,7 @@
 ﻿namespace Platform.Random
 {
-    /// <remarks>
-    /// TODO: Возможно нужна отдельная точка доступа к фабрикам по их типу.
-    /// Например Factory[Random].Default чтобы обратиться к настраиваемой (переопределяемой) фабрике по умолчанию для этого типа.
-    /// </remarks>
     public static class RandomHelpers
     {
-        public static readonly System.Random Default = new System.Random((int)System.DateTime.UtcNow.Ticks);
+        public static readonly System.Random Default = new System.Random(System.DateTime.UtcNow.Ticks.GetHashCode());
     }
 }
