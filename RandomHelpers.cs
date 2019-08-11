5b2c0b2ccc1eb125ec61bd91df4d7b87e2d79b82
Константин Дьяченко <konard@me.com>
2019-08-11 20:14:42 +0300

    Added code comments.

--- RandomHelpers.cs
+++ RandomHelpers.cs
@@ -2,6 +2,10 @@
 {
     public static class RandomHelpers
     {
+        /// <summary>
+        /// Returns the pseudorandom number generator that is using the time of the first access to this field as seed.
+        /// Возвращает генератор псевдослучайных чисел использующий в качестве seed время первого обращения к этому полю.
+        /// </summary>
         public static readonly System.Random Default = new System.Random(System.DateTime.UtcNow.Ticks.GetHashCode());
     }
 }
