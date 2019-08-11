b9d9de11fc29c3f2c88461ddafcdf0796073d6b6
linksplatform-docs <konard@yandex.ru>
2019-08-11 18:05:24 +0000

    Deploy to GitHub Pages: a40affa7b95c29edb2a121914c4b74b1e73db658

---
+++ README.md
@@ -0,0 +1,27 @@
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/f9ecd381a92445fb8c370c313a0956cb)](https://app.codacy.com/app/drakonard/Random?utm_source=github.com&utm_medium=referral&utm_content=linksplatform/Random&utm_campaign=Badge_Grade_Dashboard)
+[![CodeFactor](https://www.codefactor.io/repository/github/linksplatform/random/badge)](https://www.codefactor.io/repository/github/linksplatform/random)
+
+# [Random](https://github.com/linksplatform/Random)
+
+LinksPlatform's Platform.Random Class Library.
+
+Namespace: [Platform.Random](https://linksplatform.github.io/Random/api/Platform.Random.html)
+
+Forked from: [Konard/LinksPlatform/Platform/Platform.Helpers/Random](https://github.com/Konard/LinksPlatform/tree/9754f8e5767067176e972361a9e88ebf607d9067/Platform/Platform.Helpers/Random)
+
+NuGet package: [Platform.Random](https://www.nuget.org/packages/Platform.Random)
+
+## [Documentation](https://linksplatform.github.io/Random/)
+[PDF file](https://linksplatform.github.io/Random/Platform.Random.pdf) with code for e-readers.
+
+## Depend on
+* [Platform.Ranges](https://github.com/linksplatform/Ranges)
+
+## Mystery files
+* [.travis.yml](https://github.com/linksplatform/Random/blob/master/.travis.yml) - Travis CI build configuration.
+* [docfx.json](https://github.com/linksplatform/Random/blob/master/docfx.json) and [toc.yml](https://github.com/linksplatform/Random/blob/master/toc.yml) - DocFX build configuration.
+* [format-document.sh](https://github.com/linksplatform/Random/blob/master/format-document.sh) - script for formating `tex` file for generating PDF from it.
+* [format-csharp-files.py](https://github.com/linksplatform/Random/blob/master/format-csharp-files.py) - script for formating single `.cs` file as a part of `tex` file.
+* [generate-pdf.sh](https://github.com/linksplatform/Random/blob/master/generate-pdf.sh) - script that generates PDF with code for e-readers.
+* [publish-docs.sh](https://github.com/linksplatform/Random/blob/master/publish-docs.sh) - script that publishes generated documentation and PDF with code for e-readers to `gh-pages` branch.
+* [push-nuget.bat](https://github.com/linksplatform/Random/blob/master/push-nuget.bat) - Windows script for publishing current version of NuGet package.
---
+++ RandomExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using Platform.Ranges;
+
+namespace Platform.Random
+{
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random 64-bit unsigned integer that is greater than or equal to ulong.MinValue, and less than or equal to ulong.MaxValue.
+        /// Возвращает случайное 64-разрядное целое число без знака, которое больше или равно ulong.MinValue и меньше или равно ulong.MaxValue.
+        /// </summary>
+        /// <param name="rnd">A pseudo-random number generator. Генератор псевдослучайных чисел.</param>
+        /// <returns>A 64-bit unsigned integer that is greater than or equal to ulong.MinValue, and less than or equal to ulong.MaxValue. 64-разрядное целое число без знака, которое больше или равно ulong.MinValue и меньше или равно ulong.MaxValue.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong NextUInt64(this System.Random rnd) => rnd.NextUInt64(new Range<ulong>(ulong.MinValue, ulong.MaxValue));
+
+        /// <summary>
+        /// Returns a random 64-bit unsigned integer that is greater than or equal to minimum of specified range, and less than or equal to maximum of specified range.
+        /// Возвращает случайное 64-разрядное целое число без знака, которое больше или равно минимуму указанного диапазона и меньше или равно максимуму указанного диапазона.
+        /// </summary>
+        /// <param name="rnd">A pseudo-random number generator. Генератор псевдослучайных чисел.</param>
+        /// <param name="range">The range of possible values. Диапазон возможных значений.</param>
+        /// <returns>A 64-bit unsigned integer that is greater than or equal to the minimum of specified range, and less than or equal to the maximum of the specified range. 64-разрядное целое число без знака, которое больше или равно минимуму указанного диапазона и меньше или равно максимуму указанного диапазона.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong NextUInt64(this System.Random rnd, Range<ulong> range) => (ulong)(rnd.NextDouble() * (range.Maximum - range.Minimum)) + range.Minimum;
+
+        /// <summary>
+        /// Return a random boolean value.
+        /// Возвращает случайное булево значение.
+        /// </summary>
+        /// <param name="rnd">A pseudo-random number generator. Генератор псевдослучайных чисел.</param>
+        /// <returns>A random boolean value. Случайное булево значение.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool NextBoolean(this System.Random rnd) => rnd.Next(2) == 1;
+    }
+}
