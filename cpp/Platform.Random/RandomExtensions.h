8664825218f3d73970092aa0abcdfea168c1ab99
Stepan Ilyin <norwend@tuta.io>
2021-06-22 10:16:34 +0300

    replaced const & with just &

--- cpp/Platform.Random/RandomExtensions.h
+++ cpp/Platform.Random/RandomExtensions.h
@@ -1,16 +1,16 @@
 ﻿namespace Platform::Random
 {
-    static std::uint64_t NextUInt64(const std::mt19937_64& random)
+    static std::uint64_t NextUInt64(std::mt19937_64& random)
     {
         return NextUInt64(random, Range::UInt64);
     }
 
-    static std::uint64_t NextUInt64(const std::mt19937_64& random, const Range<std::uint64_t>& range)
+    static std::uint64_t NextUInt64(std::mt19937_64& random, const Range<std::uint64_t>& range)
     {
         return std::uniform_int_distribution<std::uint64_t>(range.Minimum, range.Maximum)(random);
     }
 
-    static bool NextBoolean(const std::mt19937_64& random)
+    static bool NextBoolean(std::mt19937_64& random)
     {
         return std::uniform_int_distribution<int>(0, 1)(random);
     }
